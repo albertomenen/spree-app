e785774b0658401eb9b26d314a7f00e680c2d607
Alberto <alberto.menendez89@gmail.com>
2024-09-16 15:20:24 +0200

    variations in cipher key and docker

--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,5 @@
+# config/environment.rb
+
 # Load the Rails application.
 require_relative "application"
 
@@ -12,6 +14,13 @@ Rails.application.initialize!
            else
              raise "No se encontró una clave de cifrado válida"
            end
+
+# Asegúrate de que la clave es de 16 bytes
+if @secret.length != 16
+  raise "La clave de cifrado debe tener exactamente 16 bytes"
+end
+
+puts "Longitud de la clave de cifrado: #{@secret.length}" # Agrega esta línea para verificar la longitud
 
 cipher = OpenSSL::Cipher::AES.new(128, :CBC)
 cipher.encrypt
