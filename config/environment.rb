fa1dcd3359c15aa4690600fcd9a1418adcc4953e
Alberto <alberto.menendez89@gmail.com>
2024-09-16 14:58:30 +0200

    set up the enviroment.rb changes

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+cipher = OpenSSL::Cipher::AES.new(128, :CBC)
+cipher.encrypt
+cipher.key = @secret # Asegúrate de que @secret sea de 16 bytes
